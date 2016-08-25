d763c840fd3a03f33dfb8fda24aaebbad6310970
chang-ning <spiderpower02@gmail.com>
2016-08-25 07:35:58 +0000

    Add some ascii graph

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+/*
+*                             /mMMMMNNM.
+*                       `sssosdMMdyhyyhoos
+*                     ..:NNNNNNNNhso++/dmN...
+*                     MMNsyysyysymMMs+/+++NNm
+*                  yddyhhsyysyysyyyhmmmmmmMMNyho
+*                  dMMyyyyyyyyyyyyyymNMddmdddmmh:/-
+*                  dNMyyyyyyyyyyyyyymNMo+++++yyhNMs
+*               sdNo+osyysyysyysyysyyyhdmNNNNsyhMMs
+*               hMMo//yyyyys/+/////////hdmddd//oNMs
+*               hMMo+/yyysso.``  ``````osyyys  :NMs
+*               hMMo++sys`.`     oNMMMM-`.MMm  :MMs
+*               -//yhdsys```     sMMNNM-`.mNm  -mMs
+*               .-:mMMyys.`` `   +hhhhh-.`hhy  :MMs
+*               yNMMMMyys...`.`        `..   `./MMs
+*         /mmdhmsosoooNNmoo+`./hmmmmhhmmmm``-mmmmMNmm/
+*      .++sdmdhdo+++++hhhdds../yyysyyyysyyooohhhhdddms++.
+*      :MMmyyo++++++++++oMMh.`..`..`..`..`MMN+//++oyymMM:
+*      -mMdsyysy+++////++//sMMNmMMMMNNMMMM////+osyysydmM:
+*   `osyhdhsyyyyo//hdh+++++oyyyyhyyhyyhyyyddh++osyysyhhdyss`
+*   .MMNyyyyyyhhsooNMM++++++++++/+++++/++/MMNsoshhyyyyyyNMM.
+*   .MMmsyysydNMMMMNNM++++//++++//++++/+++NNMMMMNMmsyyyyNMM.
+*   .MMmsyysyyydNMMMMM////++/++/++///++/++MMMmNNyhysyysydNM.
+*   .MMNyyyyyyyyNMMNNMooooooooooooooooooooNMMMMNyyyyyyyyNMM.
+*   .MMmyyyyyyyyNMMNNMyyyyyyyyyyyyyyyyyyyyNMMMMmyyyyyyyyNMM.
+*    ``/MMMMMMMM/``mMMsyysyysyysyysyssysyyMMm``/MMNmMMMM/``
+*      .oooooooooooyyhoosyyyyyyyyyyysoooosyyyooooooooooo.
+*            ```hMMo+/+++yyyyyyyyyyyo+/++++/+NMh```
+*            sMMhyysyy////++/+yMMy///+++++/++syhMMs
+*         :yydmMhsysyyoso//omds//yddo//oososssyhmMmyy:
+*    --:::omNmmNhyyyyyyyysoshh+  +hhysoyyyyyyyyhmNNmNo:::::
+*   .NMMMMmyyyyyyyyyyyyyhMMh        hMMyyyyyyyyyyyyyymMMMMM.
+* hddyhhyhysyysyssysyysyymNh        yNMyyysyysyysyysyyyhyyhhdm
+* MMMddddddhddddddddddhdmMMh        hMMmdddddhddddddddddhhdMMM
+* MMMMMMMMMNMMMMMMMMMMNMMMMh        hMMMMMMMMNMMMMMMMMMMNNMMMM
+*/
 var Slides = require('./slide')
   , theme = require('./themes/Ptt');
 
--- slides/slide1.md
+++ slides/slide1.md
@@ -1,1 +1,19 @@
 # Hello World
+
+```
+ `.hdddddddddh.`                            ...`
+    `:sdddddddddddddy:`                    .::::oddd+::::::.
+  `/oddhyyyyyyyyyyyhdd:               :////ssssdddddyssssyd+
+ +sddds:syyyyyyyyyyyhdso`           :so++++:...+ody+-..-syh.
+ hh---. .-yyyyyyyyhddddd`         :ys/-........yyds...syh.
+ `.yyhysssyydddddddddddd`         hd+..........dh:-.shs-
+    :d+ yd   +dhhhhdddhy`       :ds...sh.---....:ds:shhhdo
+    :d+ os   :s-  .ddd-         sds/-....:o/.:::+ddd+://d/
+    .o+/`      ://+hdd:         .-ydo.++...../++++ydhhdhs-
+      -/+++++++hdddshd:           hdo.hds+.../ooooydhhddd-
+    -sssddddddd+--/ddd:           hdhy+//:...:::+dddddh/
+  `ho..`yd...+d:``-do`oh`       `hy:::yh-.....-hhds:yd`
+    -y/...dddddddys...oy`         -yyyoodddddyy:::+y-
+      /yyyyyyyyyy `yyy-                -////-  syy+
+
+```
