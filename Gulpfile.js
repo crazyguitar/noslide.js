695a9e9dd692f8a50ad364e46ca7dac803951a28
chang-ning <spiderpower02@gmail.com>
2016-09-07 10:14:31 +0000

    Revert "Test coveralls :octocat:"
    This reverts commit bf4925a1e500078f7d8feb405a73ce4ac87d822a.

--- Gulpfile.js
+++ Gulpfile.js
@@ -45,6 +45,7 @@ gulp.task('test', ['pre-test', 'lint'], () => {
 });
 
 gulp.task('coveralls', ['test'], function() {
+  if (!process.env.CI) return;
   return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
     .pipe(coveralls());
 });
