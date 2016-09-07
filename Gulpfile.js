099aece1f1731fa8b34ff278b8f0c13e8e68362b
chang-ning <spiderpower02@gmail.com>
2016-09-07 19:16:45 +0800

    Test coveralls :octocat:

--- Gulpfile.js
+++ Gulpfile.js
@@ -45,7 +45,6 @@ gulp.task('test', ['pre-test', 'lint'], () => {
 });
 
 gulp.task('coveralls', ['test'], function() {
-  if (!process.env.CI) return;
   return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
     .pipe(coveralls());
 });
